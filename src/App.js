79559697ca8ddac57cc40ae5ed12e8d4c80b3468
fawzii0x3 <mbenchikha5@gmail.com>
2022-01-26 21:24:47 +0100

    some bugs need to be fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState}  from "react";
+const Block = ({ number,click,id,loge }) => {
+  return <div className={"block"} style={{backgroundColor: loge ?"#59e391":"#FFFFFF"}} onClick={() => click(id)}>{number}</div>;
+};
+const blockS = [];
+const randomize = (e) => {
+  return 1+Math.floor(Math.random() * e);
+};
+for (let i = 0; i < 10; i++) {
+  blockS.push({ id: i, value: randomize(5), loge: false });
+}
 function App() {
+  const [blocks, setBlocks] = useState(blockS);
+  const [win, setWin] = useState(false);
+  const clickHandler=(id)=>{
+    setBlocks(oldArr=>{
+      return oldArr.map(arr=>{
+        return arr.id === id ? {...arr,loge:!arr.loge}:arr  
+      })
+    })
+    let test=blocks[id].value;
+    console.log(blocks)
+    setWin(blocks.every((a)=>{
+      console.log(a.id)
+      return ( (a.loge!==blocks[id].loge) && a.value===test )}));  
+    console.log(win)
+    }
+  const blokSetter = () => {
+    setBlocks((oldBlocks) => {
+      return oldBlocks.map((oldBlock) => {
+        return oldBlock.loge ?oldBlock :{ ...oldBlock, value: randomize(5) };
+      });
+    });
+  };
+  const Roller = () => {
+    let init = setInterval(() => blokSetter(), 12);
+    setTimeout(() => clearTimeout(init), 1200);
+  };
+  const Restart=()=>{
+    setBlocks(blocks.map((block) => ({...block,loge:false})));
+    setWin(false);
+    Roller()
+  }
+  const blockMap = blocks.map((elem) => {
+    return <Block key={elem.id} id={elem.id} number={elem.value} loge={elem.loge} 
+    click={clickHandler}/>;
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tenzies</h1>
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="container">
+        {blockMap}
+      </div>
+      <button onClick={win ?Restart:Roller}>{win ? "restart ?" : "Roll"}</button>
     </div>
   );
 }
